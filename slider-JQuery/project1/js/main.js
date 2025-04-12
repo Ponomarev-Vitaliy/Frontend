6e604945782a9b3a428d4d830f40204e8356b059
Виталий Пономарев <105150849+Ponomarev-Vitaliy@users.noreply.github.com>
2025-04-12 15:30:56 +0600

    Первые проекты
    Начал новый сайт

---
+++ slider-JQuery/project1/js/main.js
@@ -0,0 +1,20 @@
+// $(document).ready(function(){
+//   $('.your-class').slick({
+//     setting-name: setting-value
+//   });
+// });
+
+// $(function(){
+
+    
+// });
+
+
+
+
+$(function(){
+  $('.slider').slick({
+    arrows:false,
+    dots:true,
+  });
+});
